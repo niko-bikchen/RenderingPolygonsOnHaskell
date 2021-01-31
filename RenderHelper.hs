25b1ff94880af0f450531e0390c5befdf81aaa23
Nikolay Bikchentaev <niko.bikcheb@gmail.com>
2021-01-31 23:16:49 +0200

    Function for surface normal calculation

--- RenderHelper.hs
+++ RenderHelper.hs
@@ -4,6 +4,13 @@ import Data.IORef
 import Graphics.UI.GLUT
 import OrbitPointOfView
 
+data MyPoint = MyPoint
+  { xC :: GLfloat,
+    yC :: GLfloat,
+    zC :: GLfloat
+  }
+  deriving (Show)
+
 red, blue, green, yellow, brown, orange, white :: Color3 GLfloat
 red = Color3 1.0 0.0 0.0
 blue = Color3 0.0 0.0 1.0
@@ -12,6 +19,30 @@ yellow = Color3 1.0 1.0 0.0
 brown = Color3 0.5 0.35 0.05
 orange = Color3 1.0 0.65 0.0
 white = Color3 1.0 1.0 1.0
+
+normilizeVector :: Normal3 GLfloat -> Normal3 GLfloat
+normilizeVector (Normal3 x y z) = Normal3 (x / magnitude) (y / magnitude) (z / magnitude)
+  where
+    magnitude = sqrt ((x * x) + (y * y) + (z * z))
+
+tupleToPoint :: (GLfloat, GLfloat, GLfloat) -> MyPoint
+tupleToPoint (x, y, z) = MyPoint {xC = x, yC = y, zC = z}
+
+makePointPairs :: [(GLfloat, GLfloat, GLfloat)] -> Int -> [(MyPoint, MyPoint)] -> [(MyPoint, MyPoint)]
+makePointPairs vertices currentArrayIndex result
+  | currentArrayIndex >= length vertices = result
+  | otherwise = makePointPairs vertices (currentArrayIndex + 1) (result ++ [(current, next)])
+  where
+    current = tupleToPoint $ vertices !! currentArrayIndex
+    next = tupleToPoint $ vertices !! mod (currentArrayIndex + 1) (length vertices)
+
+calculateSurfaceNormall :: [(GLfloat, GLfloat, GLfloat)] -> Normal3 GLfloat
+calculateSurfaceNormall points = normilizeVector (Normal3 nX nY nZ)
+  where
+    pointPairs = makePointPairs points 0 []
+    nX = foldl (\res (current, next) -> res + ((yC current - yC next) * (zC current + zC next))) 0.0 pointPairs
+    nY = foldl (\res (current, next) -> res + ((zC current - zC next) * (xC current + xC next))) 0.0 pointPairs
+    nZ = foldl (\res (current, next) -> res + ((xC current - xC next) * (yC current + yC next))) 0.0 pointPairs
 
 renderVerticesAs :: PrimitiveMode -> [(GLfloat, GLfloat, GLfloat)] -> IO ()
 renderVerticesAs primitiveShape points = renderPrimitive primitiveShape $ mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) points
